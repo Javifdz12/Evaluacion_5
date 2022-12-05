de5dc1fdd77bbc0ca5d88e2be35706b297225101
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-12-05 19:22:21 +0100

    gg

--- ejercicio2/reloj.py
+++
@@ -1,5 +0,0 @@
-import time
-import helpers
-
-helpers.limpiar_pantalla()
-print(time.strftime("%H: %M: %S"))
---
+++ ejercicio4/reloj.py
@@ -0,0 +1,11 @@
+import time
+import helpers
+while True:
+    helpers.limpiar_pantalla()
+    print('-----------')
+    print(time.strftime("%H: %M: %S"))
+    print('-----------')
+    time.sleep(1)
+
+
+
